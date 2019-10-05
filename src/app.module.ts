2a1364ab17f4b82cd86daa67c4b652733fb269dc
thinhnnd <16521179@gm.uit.edu.vn>
2019-10-05 15:10:15 +0700

    Setup DataBase and Env Variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+
+//   "entities": ["./src/**/*.entity.ts", "./dist/**/*.entity.js"]
+
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+// const dotenv = require('dotenv');
+// import dotenv from 'dotenv'  
+
+require('dotenv').config();
+import "reflect-metadata";
+
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
+
+import { Logger } from '@nestjs/common';
+
+// dotenv.config();
+const port = process.env.PORT || 8080;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
+  Logger.log(`Server listening on http://localhost:${port}`, 'Bootstrap');
 }
 bootstrap();
