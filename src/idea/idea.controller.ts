5ac8d2acb91faa7b5d43778797f55ac16df24913
thinhnnd <16521179@gm.uit.edu.vn>
2019-10-17 08:34:08 +0700

    validation pipe, log for create and update

--- src/idea/idea.controller.ts
+++ src/idea/idea.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param, UsePipes } from '@nestjs/common';
 import { IdeaService } from './idea.service';
 import { IdeaDTO } from './idea.dto';
-
-
+import { ValidationPipe} from '../shared/validation.pipe';
+import { Logger} from '@nestjs/common';
 @Controller('idea')
 export class IdeaController {
+    private logger = new Logger('IdeaController');
+
     constructor(private ideaService: IdeaService) {}
 
     @Get()
@@ -13,7 +15,9 @@ export class IdeaController {
     }
 
     @Post()
+    @UsePipes(new ValidationPipe)
     createIdea(@Body() data: IdeaDTO) {
+        this.logger.log(JSON.stringify(data));
         return this.ideaService.create(data)
     }
 
@@ -23,7 +27,9 @@ export class IdeaController {
     }
 
     @Put(':id') 
+    @UsePipes(new ValidationPipe)
     updateIdea(@Param('id') id: string,@Body() data: Partial<IdeaDTO>) {
+        this.logger.log(JSON.stringify(data));
         return this.ideaService.update(id, data)
     }   
     
